c872c7c21b3ad89b30766543d56c8b3e9e06b97b
Graham <admin@themessagedoor.com>
2025-04-05 09:21:22 -0700

    [feature] linked to Firebase

--- TheMessageDoor/TheMessageDoorApp.swift
+++ TheMessageDoor/TheMessageDoorApp.swift
@@ -6,9 +6,16 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct TheMessageDoorApp: App {
+    
+    init() {
+        FirebaseApp.configure()
+        print("Configured Firebase")
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
