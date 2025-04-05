7d71601633a6c62ba01a46d6b9ed9672f67f19cd
Graham <admin@themessagedoor.com>
2025-04-05 08:54:45 -0700

    Initial Commit

---
+++ TheMessageDoor/TheMessageDoorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheMessageDoorApp.swift
+//  TheMessageDoor
+//
+//  Created by Graham Tickell on 4/5/25.
+//
+
+import SwiftUI
+
+@main
+struct TheMessageDoorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TheMessageDoorTests/TheMessageDoorTests.swift
@@ -0,0 +1,17 @@
+//
+//  TheMessageDoorTests.swift
+//  TheMessageDoorTests
+//
+//  Created by Graham Tickell on 4/5/25.
+//
+
+import Testing
+@testable import TheMessageDoor
+
+struct TheMessageDoorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
