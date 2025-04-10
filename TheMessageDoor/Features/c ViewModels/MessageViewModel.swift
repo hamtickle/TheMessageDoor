9196cb6e29664164da667c7362645e7635116f15
Graham <admin@themessagedoor.com>
2025-04-10 09:35:38 -0700

    [feature] can write messages, but buggy
    Recipient picker not working, font picker not preselected, keyboard not collapsing

---
+++ TheMessageDoor/Features/c ViewModels/MessageViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  MessageViewModel.swift
+//  TheMessageDoor
+//
+//  Created by Graham Tickell on 4/9/25.
+//
+
+import Foundation
---
+++ TheMessageDoor/Features/e Utilities/Fonts.swift
@@ -0,0 +1,8 @@
+//
+//  Fonts.swift
+//  TheMessageDoor
+//
+//  Created by Graham Tickell on 4/9/25.
+//
+
+import Foundation
