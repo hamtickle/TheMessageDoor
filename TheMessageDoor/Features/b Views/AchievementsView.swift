dcc2e715f9c2f15af704b249c3bec57992fdef25
Graham <admin@themessagedoor.com>
2025-04-16 16:45:51 -0700

    [clean] removed repetitive db calls
    passed VMs between views and removed "get user"

---
+++ TheMessageDoor/Features/b Views/AchievementsView.swift
@@ -0,0 +1,18 @@
+//
+//  AchievementsView.swift
+//  TheMessageDoor
+//
+//  Created by Graham Tickell on 4/16/25.
+//
+
+import SwiftUI
+
+struct AchievementsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AchievementsView()
+}
